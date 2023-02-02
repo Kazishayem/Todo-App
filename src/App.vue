eb97e8231543da376bfc255fdcbbfb8a7d7735c3
Kazishayem <kazishayem1234@gmail.com>
2023-02-02 20:54:32 +0600

    ui design

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,61 @@
 <template>
 
   <div class="container">
-    <h1 class="text-bg-danger">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, sit! Cupiditate magni
-      neque maiores quae nemo placeat possimus fugit quod! Temporibus esse quod et molestiae qui optio autem accusantium
-      repellat?</h1>
+    <div class="row my-5">
+
+      <div class="col-md-12">
+
+        <div class="card">
+
+          <div class="card-body">
+
+            <h4 class="card-title text-center">Task List</h4>
+
+            <div class="row mb-3">
+
+              <form action="">
+
+                <div class="col-md-12">
+
+                  <label for="" class="form-label">Add Task</label>
+
+                  <input type="text" class="form-control">
+
+                  <div class="mt-3">
+
+
+                    <input type="button" class="btn btn-success" value="add task">
+
+                  </div>
+                </div>
+
+
+              </form>
+            </div>
+
+          </div>
+
+
+          <div class="card-body">
+            <h4 class="text-danger">Task List</h4>
+
+            <ul class="list-group">
+
+              <li class="list-group-item d-flex justify-content-between align-items-center">List Item
+
+
+              <a href="" class="btn">
+              <i class="fa-solid fa-xmark"></i>
+              </a>
+
+            </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
 
   </div>
 
