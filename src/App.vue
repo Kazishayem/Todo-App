e1eefab3115c8fde8133334102440750268e92cd
Kazishayem <kazishayem1234@gmail.com>
2023-02-02 19:59:46 +0600

    todo-app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, sit! Cupiditate magni neque maiores quae nemo placeat possimus fugit quod! Temporibus esse quod et molestiae qui optio autem accusantium repellat?</p>
+ 
+</template>
+
+<style scoped>
+
+</style>
